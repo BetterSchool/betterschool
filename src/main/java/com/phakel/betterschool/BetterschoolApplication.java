6a0bca2d1f8ea7d8c79f8d7a4262fb3e5c4873b4
EvanLuo42 <ziyun.luo@icloud.com>
2022-07-13 10:20:57 +0800

    feat: Init commit

---
+++ src/main/java/com/phakel/betterschool/BetterschoolApplication.java
@@ -0,0 +1,13 @@
+package com.phakel.betterschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterschoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BetterschoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/phakel/betterschool/BetterschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phakel.betterschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterschoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
