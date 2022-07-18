e0750f5c6b28689977ff323adb8e96b9c2123c9f
EvanLuo42 <ziyun.luo@icloud.com>
2022-07-18 22:02:59 +0800

    fix: Unit test issue
    Closes: #5

--- src/test/java/com/phakel/betterschool/UserTest.java
+++ src/test/java/com/phakel/betterschool/UserTest.java
@@ -1,12 +1,16 @@
 package com.phakel.betterschool;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.phakel.betterschool.controller.UserController;
 import com.phakel.betterschool.entity.User;
 import com.phakel.betterschool.form.RegisterForm;
+import com.phakel.betterschool.form.RegisterTestForm;
+import com.phakel.betterschool.repository.UserRepository;
 import com.phakel.betterschool.service.CommonService;
 import com.phakel.betterschool.service.UserService;
 import com.phakel.betterschool.util.I18nUtil;
+import com.phakel.betterschool.util.RequestBuildUtil;
 import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,16 +18,19 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.Locale;
 
 /**
@@ -33,40 +40,38 @@ import java.util.Locale;
 @SpringBootTest
 @RunWith(MockitoJUnitRunner.class)
 public class UserTest {
+    @Autowired
+    private WebApplicationContext wac;
+
     private MockMvc mockMvc;
 
-    @Mock
-    UserService userService;
+    @Autowired
+    private UserService userService;
 
-    @Mock
-    CommonService commonService;
+    @Autowired
+    private CommonService commonService;
 
-    @Mock
-    I18nUtil i18nUtil;
+    @Autowired
+    private I18nUtil i18nUtil;
 
     @InjectMocks
-    UserController userController;
+    private UserController userController;
 
 
     @BeforeEach
     public void setupMockMvc() {
-        mockMvc = MockMvcBuilders.standaloneSetup(userController)
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac)
                 .build();
     }
 
     @Test
     public void registerTest() throws Exception {
-        RegisterForm form = new RegisterForm();
-        form.setUserType(User.UserType.STUDENT);
+        RegisterTestForm form = new RegisterTestForm();
+        form.setUserType(User.UserType.STUDENT.name());
         form.setPassword("test-password");
         form.setUsername("test-username");
 
-        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
-                .post("/user/register")
-                .content(new ObjectMapper().writeValueAsString(form))
-                .contentType(MediaType.APPLICATION_JSON)
-                .locale(Locale.CHINA);
-        MockHttpServletResponse response = this.mockMvc.perform(requestBuilder)
+        MockHttpServletResponse response = mockMvc.perform(RequestBuildUtil.buildRequestWithBody(form, "/user/register"))
                 .andReturn()
                 .getResponse();
 
@@ -76,6 +81,6 @@ public class UserTest {
         result.setData(userService.findUserByUsername("test-username").orElseThrow());
 
         Assert.assertEquals(response.getStatus(), HttpStatus.OK.value());
-        Assert.assertEquals(response.getContentAsString(), new ObjectMapper().writeValueAsString(result));
+        Assert.assertEquals(response.getContentAsString(StandardCharsets.UTF_8), new ObjectMapper().writeValueAsString(result));
     }
 }
